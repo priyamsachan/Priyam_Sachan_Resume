e04c9059c88f55bd41cded75bccf3ab9335e0419
Priyam Sachan <72080208+priyamsachan@users.noreply.github.com>
2026-02-11 22:53:11 +0530

    Update Priyam_Sachan_Resume.py

--- Priyam_Sachan_Resume.py
+++ Priyam_Sachan_Resume.py
@@ -94,7 +94,6 @@ with st.sidebar:
     st.markdown("## Strategic Thinking, Mentorship, Technical Collaboration")
     
     st.divider()
-    st.download_button("📂 Get Official Resume", "PDF_DATA", file_name="Priyam_Sachan_Resume.pdf", use_container_width=True)
 
 # --- HERO SECTION ---
 st.title("Senior Data Engineer")
